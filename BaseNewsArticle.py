984bae095e9d0cd15fc41c7f1eaf8544ef33b925
Abhishek Saxena <5abhisheksaxena@gmail.com>
2020-02-08 05:59:21 +0530

    Add files via upload

---
+++ BaseNewsArticle.py
@@ -0,0 +1,30 @@
+from textblob import TextBlob
+
+
+class BaseNewsArticle:
+    HEADING = 0
+    ARTICLE = 1
+    SUMMARY = 3
+
+    def __init__(self, article: str, heading="", summary=""):
+        self.heading = heading
+        self.article = article  # .replace("[^a-zA-Z]", " ")
+        self.summary = summary
+
+    def getHeading(self):
+        return str(self.heading)
+
+    def getArticle(self):
+        return str(self.article)
+
+    def getSummary(self):
+        return str(self.summary)
+
+    def getTextBlob(self, type=ARTICLE) -> TextBlob:
+        return {self.HEADING: TextBlob(self.getHeading()),
+                self.ARTICLE: TextBlob(self.getArticle()),
+                self.SUMMARY: TextBlob(self.getSummary())
+                }[type]
+
+    def getTotalNumberOfWords(self, type=ARTICLE):
+        return len(self.getTextBlob(type).words)
